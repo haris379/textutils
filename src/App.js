9a6d1f585f616af12f9a908efb13022d422a2035
Muhammad Haris <sheikharis574@gmail.com>
2025-07-25 01:39:03 +0500

    ddd

--- src/App.js
+++ src/App.js
@@ -4,17 +4,7 @@ let name = "Muhammad Haris Arif"
 function App() {
   return (
     <>
-    <h1>Hello From {name}</h1>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </nav>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure,
-       nihil atque! Incidunt, deserunt temporibus recusandae qui 
-       voluptatibus explicabo quidem exercitationem earum, doloribus
-        asperiores aperiam vel aspernatur accusamus,
-       accusantium consequuntur reiciendis?</p>
+    
     </>
   );
 }
