3833c5e9afc7884ff598b7c5cac1bf0b034585ce
Muhammad Haris <sheikharis574@gmail.com>
2025-07-29 20:24:46 +0500

    Nothig to commit

--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -13,16 +13,26 @@ export default function TextForm(props) {
   };
   const clearClick = () => {
     // console.log("You clicked handlelowClick " + text);
-    let newTextClear = ' '; 
+    let newTextClear = " ";
     setText(newTextClear);
   };
+  const copyText = () => {
+    console.log("I am copy");
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+  const extraSpace = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "))
+  }
   const handleOnchange = (event) => {
     setText(event.target.value);
   };
-  const speak = () =>{
+  const speak = () => {
     let msg = new SpeechSynthesisUtterance(text);
     speechSynthesis.speak(msg);
-  }
+  };
 
   const [text, setText] = useState("");
   //   text = "Hello"; wrong way to change the state
@@ -40,10 +50,29 @@ export default function TextForm(props) {
             rows="8"
           ></textarea>
         </div>
-        <button className="btn btn-primary mx-3" onClick={handleUpClick}>Convert to Uppercase</button>
-        <button className="btn btn-primary mx-3" onClick={handlelowClick}>Convert to Lowercase</button>
-        <button className="btn btn-primary mx-3" onClick={clearClick}>Clear </button>
-        <button type="submit"  onClick={speak} className="btn btn-warning mx-2 my-2"> 🔊 Speak </button>
+        <button className="btn btn-primary mx-3" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-3" onClick={handlelowClick}>
+          Convert to Lowercase
+        </button>
+        <button className="btn btn-primary mx-3" onClick={clearClick}>
+          Clear Text{" "}
+        </button>
+        <button className="btn btn-primary mx-3" onClick={copyText}>
+          Copy Text
+        </button>
+        <button className="btn btn-primary mx-3" onClick={extraSpace}>
+          Remove Extra Spaces
+        </button>
+        <button
+          type="submit"
+          onClick={speak}
+          className="btn btn-warning mx-2 my-2"
+        >
+          {" "}
+          🔊 Speak{" "}
+        </button>
       </div>
       <div className="container my-3">
         <h2>Your text summary</h2>
