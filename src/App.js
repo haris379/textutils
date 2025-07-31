8a17c3c9fe3b8aa8792fc86994df67e715354c2a
Muhammad Haris <sheikharis574@gmail.com>
2025-07-31 14:58:28 +0500

    Just added some Functionalities

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
+import Alert from "./Alert";
 import "./App.css";
 import Navbar from "./component/Navbar";
 import TextForm from "./component/TextForm";
-// import AppCounter from "./component/AppCounter";
-// import About from "./component/About";
+import { useState } from "react";
 
 function App() {
+  const [mode , setMode] = useState('light')
+  const [alert , setAlert] = useState(null)
+
+  const showAlert = ( message , type) => {
+    setAlert({
+      msg : message,
+      type : type
+    }) 
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+    }
+
+  const toggleMode = () => {
+    if ( mode === 'light'){
+      setMode(`dark`);
+      document.body.style.backgroundColor = '#042743'
+      showAlert("Dark Mode is enabled" , "success")
+    } else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      showAlert("Light Mode is enabled" , "success")
+    }
+  }
+
   return (
     <>
       {/* <Navbar title="TextUtils" AboutText="About TextUtils" /> */}
+      {/* <Navbar/> */}
+      <Navbar title = "TextUtils"  mode = {mode}  toggleMode = {toggleMode}/>
+       <Alert alert = {alert}/>
       <div className="container my-3">
-        <TextForm heading="Enter the Text to analyze" />
-        {/* <About/> */}
+        <TextForm showAlert = {showAlert} heading="Enter the Text to analyze" mode = {mode} />
       </div>
     </>
   );
--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -1,87 +1,75 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
+
   const handleUpClick = () => {
-    // console.log("You clicked handleUpClick " + text);
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert("Converted to uppercase!" , "success");
   };
+
   const handlelowClick = () => {
-    // console.log("You clicked handlelowClick " + text);
     let newTextLower = text.toLowerCase();
     setText(newTextLower);
+    props.showAlert("Converted to lowercase!" , "success");
   };
+
   const clearClick = () => {
-    // console.log("You clicked handlelowClick " + text);
     let newTextClear = " ";
     setText(newTextClear);
+    props.showAlert("Text has been clear" , "success");
   };
+
   const copyText = () => {
     console.log("I am copy");
     var text = document.getElementById("myBox");
     text.select();
     navigator.clipboard.writeText(text.value);
+    props.showAlert("Text copy to clipboard" , "success");
   };
+
   const extraSpace = () => {
     let newText = text.split(/[ ]+/);
     setText(newText.join(" "))
+    props.showAlert("Extra spaces has been removed" , "success");
   }
+
   const handleOnchange = (event) => {
     setText(event.target.value);
   };
+
   const speak = () => {
     let msg = new SpeechSynthesisUtterance(text);
     speechSynthesis.speak(msg);
   };
 
+  // text-${props.mode === 'dark'? 'white': '#042743'}
   const [text, setText] = useState("");
   //   text = "Hello"; wrong way to change the state
   //   setText("New Text"); correct way to change the state
   return (
     <>
-      <div className="container">
+      <div className="container" style={{ color : props.mode === 'dark'? 'white': '#042743'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-          <textarea
-            className="form-control"
-            value={text}
-            onChange={handleOnchange}
-            id="myBox"
-            rows="8"
-          ></textarea>
+          <textarea  className="form-control" value={text}  onChange={handleOnchange}  
+          style={{ backgroundColor : props.mode === 'dark'? 'grey': 'white' , 
+          color :  props.mode === 'dark'? 'white': '#042743' }} 
+           id="myBox"  rows="8"></textarea>
         </div>
-        <button className="btn btn-primary mx-3" onClick={handleUpClick}>
-          Convert to Uppercase
-        </button>
-        <button className="btn btn-primary mx-3" onClick={handlelowClick}>
-          Convert to Lowercase
-        </button>
-        <button className="btn btn-primary mx-3" onClick={clearClick}>
-          Clear Text{" "}
-        </button>
-        <button className="btn btn-primary mx-3" onClick={copyText}>
-          Copy Text
-        </button>
-        <button className="btn btn-primary mx-3" onClick={extraSpace}>
-          Remove Extra Spaces
-        </button>
-        <button
-          type="submit"
-          onClick={speak}
-          className="btn btn-warning mx-2 my-2"
-        >
-          {" "}
-          🔊 Speak{" "}
-        </button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handlelowClick}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={clearClick}>Clear Text{" "}</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={copyText}>Copy Text</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={extraSpace}>Remove Extra Spaces</button>
+        <button  type="submit" onClick={speak} className="btn btn-warning mx-2 my-2" >{" "}🔊 Speak{" "}</button>
       </div>
-      <div className="container my-3">
+      <div className="container my-3" style={{ color : props.mode === 'dark'? 'white': '#042743'}} >
         <h2>Your text summary</h2>
-        <p>
-          {text.split(" ").length} words and , {text.length} characters
-        </p>
+        <p>{text.split(" ").length} words and , {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} minutes to read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text: "Enter something in the textbox above to preview it here."}</p>
       </div>
     </>
   );
